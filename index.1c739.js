dabe41403c9b0d9ac67cc3cd1a6a54a3a77c441a
kaixinxiong <126167057+kaixinxiong@users.noreply.github.com>
2023-02-23 22:26:01 +0800

    Add files via upload

---
+++ index.1c739.js
@@ -0,0 +1,25 @@
+System.register(["./application.895b4.js"], function (_export, _context) {
+  "use strict";
+
+  var Application, application;
+
+  function topLevelImport(url) {
+    return System["import"](url);
+  }
+
+  return {
+    setters: [function (_application895b4Js) {
+      Application = _application895b4Js.Application;
+    }],
+    execute: function () {
+      application = new Application();
+      topLevelImport('cc').then(function (engine) {
+        return application.init(engine);
+      }).then(function () {
+        return application.start();
+      })["catch"](function (err) {
+        console.error(err);
+      });
+    }
+  };
+});
